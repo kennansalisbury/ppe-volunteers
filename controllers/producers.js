12c96a1bd8046dd923d58f16bbe79690189f1005
kennansalisbury <kennansalisbury@gmail.com>
2020-03-23 16:59:17 -0700

    routes for producers and products controllers

--- controllers/producers.js
+++ controllers/producers.js
@@ -1,51 +1,142 @@
 const DB = require('../models');
 const ROUTER = require('express').Router();
 
-// GET /producers - find all producers
+// GET /producers - find all ACTIVE producers and provide info depending on which type of user the rq is coming from
 ROUTER.get('/', (req, res) => {
-    //stub for testing route
-    res.send('find all producers')
+    //req.headers from front end to include user the request is coming from
+        //include type of user (i.e. isClinic, isAdmin etc.), and user region
+    
+    //if rq coming from source other than admin, producer lead, or clinic lead - forbidden
+    if(req.headers['isadmin'] === 'false' && req.headers['isprodlead'] === 'false' && req.headers['isclinic'] === 'false') {
+        res.status(403).send({message: 'Forbidden'})
+        return
+    }
 
-    // DB.User.find({ isProducer: true })
-    // .then(users => {
-    //     res.send(users)
-    // })
-    // .catch(err => {
-    //     console.log('Error finding producers', err)
-    //     res.status(503).send({message: 'Internal server error'})
-    // })
+    DB.User.find({ isProducer: true, isActive: true })
+    .then(producers => {
+        
+        //if rq coming from admin, send all producer details
+        if(req.headers['isadmin'] === 'true') {
+            res.send(producers)
+        } 
+        //if rq coming from producer lead, send all info for producers in their region
+        else if(req.headers['isprodlead'] === 'true') {
+            let data = producers.filter(producer => producer.region === req.headers['region'])
+            res.send(data)
+        }
+        //if rq coming from clinic lead, send only id &  inventory for all producers in their region
+        else if(req.headers['isclinic'] === 'true') {
+            let list = producers.filter(producer => producer.region === req.headers['region'])
+            let data = list.map(producer => ({
+                id: producer._id,
+                inventory: producer.inventory
+            }))
+            res.send(data)
+        }
+    })
+    .catch(err => {
+        console.log('Error finding producers', err)
+        res.status(503).send({message: 'Internal server error'})
+    })
 })
 
-
-//GET /producers/:id - find 1 producer
+//GET /producers/:id - find 1 producer, show only necessary info for person viewing
 ROUTER.get('/:id', (req, res) => {
     //stub for testing route
-    res.send('find 1 producer')
+    // res.send('find 1 producer')
 
-    //need to make this more secure than params?
-    // DB.User.findById(req.params.id)
-    // .then(user => {
-    //     res.send(user) //update to only send needed fields once confirmed what we need to see
-    // })
+    //req.headers from front end to include user the request is coming from
+        //include type of user (i.e. isClinic, isAdmin etc.), user region and user id
+    
+    //forbidden request if rq coming from: 
+    //a producer whose id is different than req.params.id
+    if((req.headers['isproducer'] === 'true' && req.params.id != req.headers['id'])) { 
+        res.status(403).send({message: 'Forbidden'})
+        return
+    }
+
+    //OR from source other than any type of user
+    if(req.headers['isadmin'] === 'false' && req.headers['isprodlead'] === 'false' && req.headers['isclinic'] === 'false' && req.headers['isproducer'] === 'false' && req.headers['isdriver'] === 'false') {
+        res.status(403).send({message: 'Forbidden'})
+        return
+    }  
+
+    DB.User.findById(req.params.id)
+    .then(producer => {
+   
+        //if rq coming from producer (same id as requesting) or from admin, show all info
+        if((req.headers['isproducer'] === 'true' && req.params.id === req.headers['id']) || req.headers['isadmin'] === 'true' ) {
+            res.send(producer)
+        } 
+        //if rq coming from prod lead, show all info only if in region, otherwise forbidden
+        else if(req.headers['isprodlead'] === 'true') {
+            producer.region === req.headers['region'] ? res.send(producer) : res.status(403).send({message: 'Forbidden'})
+        } 
+        //if rq coming from clinic lead, show id and inventory only if in region, otherwise forbidden
+        else if(req.headers['isclinic'] === 'true') {
+            currentProducer = {
+                id: producer._id,
+                inventory: producer.inventory
+            }
+            producer.region === req.headers['region'] ?  res.send(currentProducer) : res.status(403).send({message: 'Forbidden'})
+        }
+        //if rq coming from driver, show id and address only
+        else if(req.headers['isdriver'] === 'true') {
+            currentProducer = {
+                id: producer._id,
+                address: producer.address,
+                city: producer.city,
+                state: producer.state,
+                zipcode: producer.zipcode
+            }
+            res.send(currentProducer)
+        }
+    })
+    .catch(err => {
+        console.log('Error finding producer', err)
+        res.status(503).send({message: 'Internal server error'})
+    }) 
 })
 
 
 //PUT /producers/:id - update 1 producer
 ROUTER.put('/:id', (req, res) => {
     //stub for testing route
-    res.send('update 1 producer')
+    // res.send('update 1 producer')
 
-    // DB.User.updateOne(
-    //     {_id: req.params.id},
-    //     {inventory: req.body.inventory} //may need to update based on how we have them update inventory
-    // )
-    // .then(updated => {
-    //     //need to be able to test and play around with what this returns
-    // })
-    // .catch(err => {
-    //     console.log('Error updating inventory', err)
-    //     res.status(503).send('Internal server error')
-    // })
+    //only can update a producer's inventory if requested by that producer or potentially if prodlead (need to search for user and determine region first)
+    if(req.headers['isproducer'] === 'true' || req.headers['isprodlead'] === 'true') {
+
+        DB.User.findById(req.params.id)
+        .then(user => {
+            //if requesting user's id is equal to the requested user id, or requesting user's region is equal to the requested user's region, update
+            if(req.params.id === req.headers['id'] || user.region === req.headers['region']) {
+                
+                //update inventory
+                DB.User.updateOne(
+                    {_id: req.params.id},
+                    {inventory: req.body.inventory} //may need to update this code based on how we have them update inventory
+                )
+                .then(updated => {
+                    res.send(updated) //front end will confirm if updated w/ response.n === 1
+                })
+                .catch(err => {
+                    console.log('Error updating inventory', err)
+                    res.status(503).send('Internal server error')
+                })
+
+            } else {
+                res.status(403).send({message: 'Forbidden'})
+            }
+        })
+        .catch(err => {
+            console.log('Error finding user', err)
+            res.status(503).send({message: 'Internal server error'})
+        })  
+    }
+    else {
+        res.status(403).send({message: 'Forbidden'})
+    }   
 
 })
 
--- controllers/products.js
+++ controllers/products.js
@@ -3,13 +3,26 @@ const ROUTER = require('express').Router();
 
 //GET  /products - All products
 ROUTER.get('/', (req, res) => {
-    res.send('show all products')
+    DB.Product.find().then(products => res.send(products))
 })
 
 
 //POST /products - Create new product (admin only)
 ROUTER.post('/', (req, res) => {
-    res.send('create new product')
+    
+    if(req.headers['isadmin'] === 'true') {
+
+        DB.Product.create({name: req.body.name})
+        .then(product => res.send({message: `${product.name} created`}))
+        .catch(err => {
+            console.log('Error creating product', err)
+            res.status(503).send({message: 'Internal server error'})
+        })
+    }
+    else {
+        res.status(403).send({message: 'Forbidden'})
+    }
+   
 })
 
 module.exports = ROUTER;
